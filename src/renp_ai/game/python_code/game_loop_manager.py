1547684617505ba0c6ec0d0919e34711460ca5e9
Shardic <eli.ronai@gmail.com>
2024-08-19 17:08:26 -0500

    images generate before text

--- src/renp_ai/game/python_code/game_loop_manager.py
+++ src/renp_ai/game/python_code/game_loop_manager.py
@@ -8,6 +8,9 @@ class GameLoopManager:
         if not self.display_manager.api_manager.api_key_manager.api_key:
             self.display_manager.run_first_time_setup()
 
+        # set the default scene
+        self.display_manager.change_scene(self.context_manager.scene.location)
+
         # choice` is a dictionary with the following structure:
         #  {"choice": "text1", "flags": ["location"]}
         choice = {
@@ -34,14 +37,14 @@ class GameLoopManager:
             # 'choice' should always be a dictionary containing 'choice' and 'flags'
             flags = choice.get('flags', [])
 
-            #  options is a list of dictionaries with the following structure:
-            #  [{"choice": "text1", "flags": ["location"]}]
-            options = self.display_manager.handle_choice_and_get_response(choice)
-
             if 'location' in flags:
                 # Inform ContextManager about the location change
                 new_location = self.context_manager.handle_location_change(choice)
                 # After the context manager updates the location, inform the display manager
                 self.display_manager.change_scene(new_location)
+
+            #  options is a list of dictionaries with the following structure:
+            #  [{"choice": "text1", "flags": ["location"]}]
+            options = self.display_manager.handle_choice_and_get_response(choice)
 
         return "end"
